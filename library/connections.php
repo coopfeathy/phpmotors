8dcf19bc87be2b736f22041030130df4d8a79fe5
coopfeathy <cooperfeatherstone13@gmail.com>
2023-02-25 12:52:04 -0700

    enhancement2

---
+++ library/connections.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Proxy connection to the database
+ */
+function phpmotorsConnect()
+{
+    $server = 'localhost';
+    $dbname = 'phpmotors';
+    $username = 'iClient';
+    $password = '';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    try{
+        $link = new PDO($dsn, $username, $password, $options);
+
+        echo 'Hey I can connect!';
+
+        return $link;
+
+    } catch(PDOException $e){
+        header('Location: /phpmotors/view/500.php');
+        exit;
+    }
+}
+
+
+phpmotorsConnect();
+?>
+
---
+++ snippets/header.php
@@ -0,0 +1,2 @@
+<img src="../images/site/logo.png" alt = "Logo">
+<p>My Account</p>
