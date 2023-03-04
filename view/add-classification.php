f405a5c37c1e57296517d008592f4ca67cb1c235
coopfeathy <cooperfeatherstone13@gmail.com>
2023-03-03 19:03:11 -0700

    enhancement 5

--- view/add-classification.php
+++ view/add-classification.php
@@ -25,7 +25,7 @@
             <form action="/phpmotors/vehicles/index.php" method="POST">
                 <label>Classification Name</label>
                 <br>
-                <input type="text" name="newClassification" id="newClassification">
+                <input type="text" name="newClassification" id="newClassification" required>
                 <br>
                 <input type="submit" name="submit" id="regbtn" value="Register">
                 <!-- Add the action name - value pair -->
